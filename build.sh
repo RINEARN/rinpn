8a9537493b3db6008850584c9867025520293ebd
RINEARN <rinearn@gmail.com>
2019-09-21 01:37:15 +0900

    Update: build scripts: rename "RinearnProcessorNano.jar" to "RINPn.jar"

--- build.sh
+++ build.sh
@@ -13,4 +13,4 @@ cd ..
 # create a JAR file
 # --------------------------------------------------
 
-jar cvfm RinearnProcessorNano.jar src/main.mf -C bin com
+jar cvfm RINPn.jar src/main.mf -C bin com
