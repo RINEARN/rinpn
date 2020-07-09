b35adf37e5bda6f508886d1443cd648f431c89f7
RINEARN <rinearn@gmail.com>
2020-07-09 19:14:59 +0900

    Add: display "ERROR" on the OUTPUT text field when an error has occurred

--- src/com/rinearn/processornano/model/AsynchronousCalculationRunner.java
+++ src/com/rinearn/processornano/model/AsynchronousCalculationRunner.java
@@ -47,12 +47,19 @@ public final class AsynchronousCalculationRunner implements Runnable {
 			return;
 		}
 
-		// 入力フィールドの計算式を実行し、結果の値を取得
-		String outputText = "";
 		try {
-			outputText = this.calculator.calculate(this.inputExpression, true, this.setting);
+			// 入力フィールドの計算式を実行し、結果の値を取得
+			String outputText = this.calculator.calculate(this.inputExpression, true, this.setting);
+
+			// 計算リクエスト元に計算完了を通知
+			this.calculationListener.calculationFinished(outputText);
 
 		} catch (ScriptException | RinearnProcessorNanoException e) {
+
+			// 計算結果の代わりに、エラーの発生を示すメッセージを通知（ OUTPUT 欄に表示される ）
+			this.calculationListener.calculationFinished("ERROR");
+
+			//エラー内容をユーザーに表示
 			String errorMessage = MessageManager.customizeExceptionMessage(e.getMessage());
 			if (setting.localeCode.equals(LocaleCode.EN_US)) {
 				MessageManager.showErrorMessage(errorMessage, "Expression/Script Error");
@@ -63,10 +70,6 @@ public final class AsynchronousCalculationRunner implements Runnable {
 			if (setting.exceptionStackTracerEnabled) {
 				MessageManager.showExceptionStackTrace(e);
 			}
-			return;
 		}
-
-		// 計算リクエスト元に計算完了を通知
-		this.calculationListener.calculationFinished(outputText);
 	}
 }
