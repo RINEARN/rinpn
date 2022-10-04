7472874e3529531b942454c8a5e2e88632ff9f4a
RINEARN <commit@rinearn.com>
2022-10-05 02:31:40 +0900

    Add: source code of the scripting engine of Vnano
    The source code of the scripting engine is maintained
    on the other repository, and anyone can get it anytime.
    So we had considered that
    there is no need to bandle it in this repository.
    However, we have changed our mind,
    and have bundled it in this repository by this commit.
    
    The main reason is:
    Each version of this app requires the corresponding
    version of the scripting engine.
    
    The secondary reason is:
    We want to make the building steps simple and easy,
    as possible as.

--- build.bat
+++ build.bat
@@ -1,14 +1,36 @@
-:: --------------------------------------------------
-:: compile source files
-:: --------------------------------------------------
+:: ==================================================
+:: A batch file for building the RINPn
+:: ==================================================
 
 mkdir bin
+
+
+:: --------------------------------------------------
+:: compile source files of the Vnano Engine
+:: --------------------------------------------------
+
+cd src
+javac @org/vcssl/nano/sourcelist.txt -d ../bin -encoding UTF-8
+cd ..
+
+:: --------------------------------------------------
+:: create a JAR file of the Vnano Engine (Vnano.jar)
+:: --------------------------------------------------
+
+jar cvfm Vnano.jar src/org/vcssl/nano/meta/main.mf -C bin org -C src/org/vcssl/nano/meta META-INF
+
+
+:: --------------------------------------------------
+:: compile source files of the RINPn
+:: --------------------------------------------------
+
 cd src
 javac @com/rinearn/processornano/sourcelist.txt -d ../bin -encoding UTF-8
 cd ..
 
 :: --------------------------------------------------
-:: create a JAR file
+:: create the JAR file of the RINPn (RINPn.jar)
 :: --------------------------------------------------
 
 jar cvfm RINPn.jar src/com/rinearn/processornano/meta/main.mf -C bin com
+
--- build.sh
+++ build.sh
@@ -1,16 +1,38 @@
 #!/bin/sh
 
-# --------------------------------------------------
-# compile source files
-# --------------------------------------------------
+# ==================================================
+# A shell script for building the RINPn
+# ==================================================
 
 mkdir bin
+
+
+# --------------------------------------------------
+# compile source files of the Vnano Engine
+# --------------------------------------------------
+
+cd src
+javac @org/vcssl/nano/sourcelist.txt -d ../bin -encoding UTF-8
+cd ..
+
+# --------------------------------------------------
+# create a JAR file of the Vnano Engine (Vnano.jar)
+# --------------------------------------------------
+
+jar cvfm Vnano.jar src/org/vcssl/nano/meta/main.mf -C bin org -C src/org/vcssl/nano/meta META-INF
+
+
+# --------------------------------------------------
+# compile source files of the RINPn
+# --------------------------------------------------
+
 cd src
 javac @com/rinearn/processornano/sourcelist.txt -d ../bin -encoding UTF-8
 cd ..
 
 # --------------------------------------------------
-# create a JAR file
+# create a JAR file of the RINPn (RINPn.jar)
 # --------------------------------------------------
 
 jar cvfm RINPn.jar src/com/rinearn/processornano/meta/main.mf -C bin com
+
