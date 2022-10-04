1a03bc68581f5226c104b1fc159dd6c59669e3ef
RINEARN <commit@rinearn.com>
2022-10-05 04:13:55 +0900

    Move: source code of the scripting engine of Vnano
    From:
      src/org/vcssl/nano...
    
    To:
      src_vnano/org/vcssl/nano...
    
    Because:
      It may be better to separate the engine's source folder
      from the app's source folder.

--- build.bat
+++ build.bat
@@ -3,21 +3,22 @@
 :: ==================================================
 
 mkdir bin
+mkdir bin_vnano
 
 
 :: --------------------------------------------------
 :: compile source files of the Vnano Engine
 :: --------------------------------------------------
 
-cd src
-javac @org/vcssl/nano/sourcelist.txt -d ../bin -encoding UTF-8
+cd src_vnano
+javac @org/vcssl/nano/sourcelist.txt -d ../bin_vnano -encoding UTF-8
 cd ..
 
 :: --------------------------------------------------
 :: create a JAR file of the Vnano Engine (Vnano.jar)
 :: --------------------------------------------------
 
-jar cvfm Vnano.jar src/org/vcssl/nano/meta/main.mf -C bin org -C src/org/vcssl/nano/meta META-INF
+jar cvfm Vnano.jar src_vnano/org/vcssl/nano/meta/main.mf -C bin_vnano org -C src_vnano/org/vcssl/nano/meta META-INF
 
 
 :: --------------------------------------------------
