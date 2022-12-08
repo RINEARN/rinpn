91b042cd50a9f1ede52c0d8d4132243813e462d8
RINEARN <commit@rinearn.com>
2022-12-08 21:42:17 +0900

    Update: Vnano Standard Plug-ins
    The following variables/functions have been added:
    
    * UP
    * UP_SIGNIF
    * DOWN
    * DOWN_SIGNIF
    * HALF_UP
    * HALF_UP_SIGNIF
    * HALF_DOWN
    * HALF_DOWN_SIGNIF
    * HALF_TO_EVEN
    * HALF_TO_EVEN_SIGNIF
    * round(float,int,int)
    * round(string,int,int)

--- plugin/org/vcssl/nano/plugin/system/xnci1/SystemDataTypeXnci1Plugin.java
+++ plugin/org/vcssl/nano/plugin/system/xnci1/SystemDataTypeXnci1Plugin.java
@@ -1,5 +1,5 @@
 /*
- * Author:  RINEARN (Fumihiro Matsui), 2020-2021
+ * Author:  RINEARN (Fumihiro Matsui), 2020-2022
  * License: CC0
  */
 
@@ -18,6 +18,8 @@ import org.vcssl.nano.plugin.system.xfci1.InfXfci1Plugin;
 import org.vcssl.nano.plugin.system.xfci1.LengthXfci1Plugin;
 import org.vcssl.nano.plugin.system.xfci1.NanXfci1Plugin;
 import org.vcssl.nano.plugin.system.xfci1.ArrayrankXfci1Plugin;
+import org.vcssl.nano.plugin.system.xfci1.StringRoundXfci1Plugin;
+import org.vcssl.nano.plugin.system.xfci1.FloatRoundXfci1Plugin;
 import org.vcssl.nano.plugin.system.xvci1.FloatMaxXvci1Plugin;
 import org.vcssl.nano.plugin.system.xvci1.FloatMinAbsDenormalXvci1Plugin;
 import org.vcssl.nano.plugin.system.xvci1.FloatMinAbsNormalXvci1Plugin;
@@ -25,9 +27,16 @@ import org.vcssl.nano.plugin.system.xvci1.InfXvci1Plugin;
 import org.vcssl.nano.plugin.system.xvci1.IntMaxXvci1Plugin;
 import org.vcssl.nano.plugin.system.xvci1.IntMinXvci1Plugin;
 import org.vcssl.nano.plugin.system.xvci1.NanXvci1Plugin;
-
-//Interface Specification: https://www.vcssl.org/en-us/dev/code/main-jimpl/api/org/vcssl/connect/ExternalNamespaceConnectorInterface1.html
-//インターフェース仕様書:  https://www.vcssl.org/ja-jp/dev/code/main-jimpl/api/org/vcssl/connect/ExternalNamespaceConnectorInterface1.html
+import org.vcssl.nano.plugin.system.xvci1.UpXvci1Plugin;
+import org.vcssl.nano.plugin.system.xvci1.UpSignifXvci1Plugin;
+import org.vcssl.nano.plugin.system.xvci1.DownXvci1Plugin;
+import org.vcssl.nano.plugin.system.xvci1.DownSignifXvci1Plugin;
+import org.vcssl.nano.plugin.system.xvci1.HalfUpXvci1Plugin;
+import org.vcssl.nano.plugin.system.xvci1.HalfUpSignifXvci1Plugin;
+import org.vcssl.nano.plugin.system.xvci1.HalfDownXvci1Plugin;
+import org.vcssl.nano.plugin.system.xvci1.HalfDownSignifXvci1Plugin;
+import org.vcssl.nano.plugin.system.xvci1.HalfToEvenXvci1Plugin;
+import org.vcssl.nano.plugin.system.xvci1.HalfToEvenSignifXvci1Plugin;
 
 public class SystemDataTypeXnci1Plugin implements ExternalNamespaceConnectorInterface1 {
 
@@ -48,6 +57,8 @@ public class SystemDataTypeXnci1Plugin implements ExternalNamespaceConnectorInte
 		functionList.add(new InfXfci1Plugin());
 		functionList.add(new LengthXfci1Plugin());
 		functionList.add(new ArrayrankXfci1Plugin());
+		functionList.add(new FloatRoundXfci1Plugin());
+		functionList.add(new StringRoundXfci1Plugin());
 		return functionList.toArray(new ExternalFunctionConnectorInterface1[0]);
 	}
 
@@ -61,6 +72,16 @@ public class SystemDataTypeXnci1Plugin implements ExternalNamespaceConnectorInte
 		variableList.add(new FloatMaxXvci1Plugin());
 		variableList.add(new FloatMinAbsNormalXvci1Plugin());
 		variableList.add(new FloatMinAbsDenormalXvci1Plugin());
+		variableList.add(new UpXvci1Plugin());
+		variableList.add(new UpSignifXvci1Plugin());
+		variableList.add(new DownXvci1Plugin());
+		variableList.add(new DownSignifXvci1Plugin());
+		variableList.add(new HalfUpXvci1Plugin());
+		variableList.add(new HalfUpSignifXvci1Plugin());
+		variableList.add(new HalfDownXvci1Plugin());
+		variableList.add(new HalfDownSignifXvci1Plugin());
+		variableList.add(new HalfToEvenXvci1Plugin());
+		variableList.add(new HalfToEvenSignifXvci1Plugin());
 		return variableList.toArray(new ExternalVariableConnectorInterface1[0]);
 	}
 
