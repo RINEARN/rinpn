8f85a47c75259d566c605f3f7836d35606f65445
RINEARN <commit@rinearn.com>
2025-10-15 22:36:42 +0900

    Update: Version: 1.0.4 (release version)

--- src/com/rinearn/rinpn/RINPn.java
+++ src/com/rinearn/rinpn/RINPn.java
@@ -21,7 +21,7 @@ import java.lang.reflect.InvocationTargetException;
  */
 public final class RINPn {
 
-	private static final String VERSION = "1.0.3";
+	private static final String VERSION = "1.0.4";
 	private static final String OPTION_NAME_VERSION = "--version";
 	private static final String OPTION_NAME_DEBUG = "--debug";
 	private static final String OPTION_NAME_DIR = "--dir";
